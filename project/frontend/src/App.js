b6cd64c943b711fd2d39bc840395156ddff82488
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2023-11-09 20:11:29 +0500

    Add some front

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, {useState} from 'react';
+import './styles/App.css';
+import PostList from './components/post_list';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
-ReactDOM.render(
-    <div>
-    Приложение воркает!
-    </div>,
-    document.getElementById('root')
-)
+function App() {
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript', body: 'It is some language'},
+        {id: 2, title: 'JavaScript 2', body: 'Mmmmmm....'},
+    ])
+
+    const [title, setTitle] = useState('')
+    const addNewPost = (e) => {
+        e.preventDefault()
+        console.log(title)
+    }
+
+    return (
+        <div className='App'>
+            <form>
+                <MyInput value={title} onChange={e => setTitle(e.target.value)} type='text' placeholder='Name of post' />
+                <MyInput type='text' placeholder='Description of post'/>
+                <MyButton onClick={addNewPost}>Create post</MyButton>
+            </form>
+
+            <PostList posts={posts} title='List of posts with JS'/>
+        </div>
+    )
+};
+
+export default App;
