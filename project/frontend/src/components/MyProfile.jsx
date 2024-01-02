b20dfb633f3231b0865bce5321a5c0ccdf190f72
Vismut <pavel.epanovm@gmail.com>
2024-01-02 23:11:06 +0500

    Add profile

--- project/frontend/src/components/MyProfile.jsx
+++ project/frontend/src/components/MyProfile.jsx
@@ -1,84 +1,26 @@
-import React, {useState, useEffect} from "react";
-import Header from "./UI/header/Header";
-import axios from "axios";
-import { useNavigate} from "react-router-dom";
 import { HeaderMegaMenu } from "./HeaderMegaMenu/HeaderMegaMenu";
 import { FooterCentered } from "./FooterCentered/FooterCentered";
-import { Paper } from "@mantine/core";
+import { Paper, Container } from "@mantine/core";
+import { UserInfo } from "./UserInfo/UserInfo";
 
 const MyProfile = (props) => {
-      const navigate = useNavigate()
-
-      const [data, setData] = useState([]);
-
-      const HomePage = () => {
-        navigate("/");
-    }
-
-      const NotLogin = () => {
-        navigate("/notlogin")
-      }
-
-      const containerStyles = {
-        position: 'fixed',
-        bottom: 0,
-        width: '100%',
-      };
 
 
-      useEffect(() => {
-        fetch("http://localhost:8000/users/profile", {method: 'GET',
-        credentials: 'include' })
-          .then(response => {
-            if (response.status === 401){
-                NotLogin()
-            }
-            else {
-                return response.json()
-            }
-          })
-          .then(jsonData => setData(jsonData))
-          .catch(error => console.error('Error fetching data:', error));
-      }, [])
-    
-    
-      
-      const deleteData = async () => {
-        try {
-          await axios.delete('http://localhost:8000/auth/logout', {withCredentials: true});
-        } catch (error) {
-          console.error('Error deleting data:', error);
-        }
-      };
-
-      const HandleButton = async () => {
-        await deleteData();
-        HomePage();
-      };
-    
-
+  const containerStyles = {
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+  };
 
     return (
         <div>
             <HeaderMegaMenu/>
-            <h2>Мой профиль</h2>
             <div>
-                <ul>
-                    {[data].map(item => (
-                    <li key={1}>Почта: {item.email} </li>
-                    ))}
-
-                    {[data].map(item => (
-                    <li key={2}>Имя: {item.name} </li>
-                    ))}
-
-                    {[data].map(item => (
-                    <li key={3}>Фамилия: {item.surname} </li>
-                    ))}
-                </ul>
-
-                <button onClick={HandleButton}>Выйти из аккаунта</button>
+              <Container>
+                <UserInfo/>
+              </Container>
             </div>
+            
 
             <div>
                 <Paper style={containerStyles}>
