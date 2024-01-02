9344123a6dbfc15f4b8c799e4eea2b7252cf8275
Vismut <pavel.epanovm@gmail.com>
2024-01-02 23:26:03 +0500

    add my courses

--- project/frontend/src/components/AllPrivateCourses.jsx
+++ project/frontend/src/components/AllPrivateCourses.jsx
@@ -1,22 +1,11 @@
-import React, {useState, useEffect} from "react";
-import Header from "./UI/header/Header";
-import { useNavigate } from "react-router-dom";
 import { HeaderMegaMenu } from "./HeaderMegaMenu/HeaderMegaMenu";
 import { FooterCentered } from "./FooterCentered/FooterCentered";
 import { Paper } from "@mantine/core";
+import { TableOfCourses } from "./TableOfCOurses/TableOfCourses";
+import { Container } from "@mantine/core";
 
 const AllPrivateCourses = () => {
-    const navigate = useNavigate()
 
-    const [data, setData] = useState([]);
-
-    const NotLogin = () => {
-      navigate("/notlogin")
-    }
-
-    const Task = (course_id) => {
-        navigate(`/courses/tasks/${course_id}`)
-    }
     
     const containerStyles = {
         position: 'fixed',
@@ -24,38 +13,19 @@ const AllPrivateCourses = () => {
         width: '100%',
       };
 
-        useEffect(() => {
-            fetch("http://localhost:8000/courses", {method: 'GET',
-            credentials: 'include' })
-              .then(response => {
-                if (response.status === 401){
-                    NotLogin()
-                }
-                else {
-                    return response.json()
-                }
-              })
-              .then(jsonData => setData(jsonData))
-              .catch(error => console.error('Error fetching data:', error));
-        }, [])
-  
+
 
     return (
         <div>
             <HeaderMegaMenu/>
 
             <div>
-                {data.map(item => (
-                    <div key={item.id}>
-                    <h2>Название: {item.title}</h2>
-                    <h3>Описание: {item.description}</h3>
-                    <p>Код курса: {item.id}</p>
-                    <button onClick={() => Task(item.id)}>Зайти в курс</button>
-                    </div>
-                ))}
+                <Container fluid>
+                    <TableOfCourses/>
+                </Container>
             </div>
-            
 
+            
             <div>
                 <Paper style={containerStyles}>
                     <FooterCentered/>
---
+++ project/frontend/src/components/TableOfCOurses/TableOfCourses.tsx
@@ -0,0 +1,67 @@
+import cx from 'clsx';
+import { Table, ScrollArea } from '@mantine/core';
+import classes from './TableOfCourses.module.css'
+import React, {useState, useEffect} from "react";
+import Header from "./UI/header/Header";
+import { useNavigate } from "react-router-dom";
+import { useMantineTheme, Button } from '@mantine/core';
+
+
+
+export function TableOfCourses() {
+  const [scrolled, setScrolled] = useState(false);
+
+  const navigate = useNavigate()
+
+  const theme = useMantineTheme();
+
+    const [data, setData] = useState([]);
+
+    const NotLogin = () => {
+      navigate("/notlogin")
+    }
+
+    const Task = (course_id) => {
+        navigate(`/courses/tasks/${course_id}`)
+    }
+    
+
+        useEffect(() => {
+            fetch("http://localhost:8000/courses", {method: 'GET',
+            credentials: 'include' })
+              .then(response => {
+                if (response.status === 401){
+                    NotLogin()
+                }
+                else {
+                    return response.json()
+                }
+              })
+              .then(jsonData => setData(jsonData))
+              .catch(error => console.error('Error fetching data:', error));
+        }, [])
+
+  const rows = data.map((row) => (
+    <Table.Tr key={row.id}>
+      <Table.Td>{row.title}</Table.Td>
+      <Table.Td>{row.description}</Table.Td>
+      <Table.Td>{row.id}</Table.Td>
+      <Table.Td><Button variant="default" onClick={() => Task(row.id)}>Зайти</Button></Table.Td>
+    </Table.Tr>
+  ));
+
+  return (
+    <ScrollArea h={300} onScrollPositionChange={({ y }) => setScrolled(y !== 0)}>
+      <Table miw={700}>
+        <Table.Thead className={cx(classes.header, { [classes.scrolled]: scrolled })}>
+          <Table.Tr>
+            <Table.Th>Название</Table.Th>
+            <Table.Th>Описание</Table.Th>
+            <Table.Th>Номер</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+    </ScrollArea>
+  );
+}
