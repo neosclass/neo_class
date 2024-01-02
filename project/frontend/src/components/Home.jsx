57f05d0c97dcd9dac1567bd08c1091e2a37581c0
Vismut <pavel.epanovm@gmail.com>
2024-01-02 21:29:32 +0500

    Add footer

--- project/frontend/src/components/Home.jsx
+++ project/frontend/src/components/Home.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import Header from "./UI/header/Header";
 import { useNavigate } from "react-router-dom";
 import { HeaderMegaMenu } from "./HeaderMegaMenu/HeaderMegaMenu";
-import { Center, Text, Container } from '@mantine/core';
+import { Center, Text, Container, Paper } from '@mantine/core';
+import { FooterCentered } from "./FooterCentered/FooterCentered";
 
 const Home = () => {
+
+    const containerStyles = {
+        position: 'fixed',
+        bottom: 0,
+        width: '100%',
+      };
+
 
     return (
         <div>
@@ -14,19 +22,20 @@ const Home = () => {
                 </Container>
             </div>
 
+
+
             <div>
-                <Container fluid>
-                    <Center>
-                    <Text style={{ fontSize: '50px' }} fw={700}>Создавай курсы и присоединяйся к ним</Text>
-                    </Center>
-                </Container>
-                <Container fluid h={50}>
-                    <Center>
-                    <Text style={{ fontSize: '20px' }} fs='italic'>Быть умным и хорошо учиться — две разные вещи. </Text>
-                    </Center>
-                </Container>
+            <Paper style={{ textAlign: 'center', padding: 200 }}>
+                <Text style={{ fontSize: '50px' }} fw={700}>Создавай курсы и присоединяйся к ним</Text>
+                <Text style={{ fontSize: '20px' }} fs='italic'>Быть умным и хорошо учиться — две разные вещи. </Text>
+            </Paper>
             </div>
 
+            <div>
+                <Paper style={containerStyles}>
+                    <FooterCentered/>
+                </Paper>
+            </div>
 
         </div>
         
