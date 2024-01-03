7ce7a84fcd078c1cbae800e1b6f4d106ed17d81d
Vismut <pavel.epanovm@gmail.com>
2024-01-03 18:21:50 +0500

    add notlogin every

--- project/frontend/src/components/CreateCourse.jsx
+++ project/frontend/src/components/CreateCourse.jsx
@@ -2,9 +2,16 @@ import { HeaderMegaMenu } from "./HeaderMegaMenu/HeaderMegaMenu";
 import { FooterCentered } from "./FooterCentered/FooterCentered";
 import { Paper, Container } from "@mantine/core";
 import { InputCreateCourse } from "./InputCreateCourse/InputCreateCourse";
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const CreateCourse = () => {
+  const navigate = useNavigate()
+
+
+  const NotLogin = () => {
+    navigate('/notlogin')
+  }
 
 
     const containerStyles = {
@@ -12,6 +19,20 @@ const CreateCourse = () => {
       bottom: 0,
       width: '100%',
     };
+
+    useEffect(() => {
+      fetch("http://localhost:8000/users/profile", {method: 'GET',
+      credentials: 'include' })
+        .then(response => {
+          if (response.status === 401){
+              NotLogin()
+          }
+          else {
+              return response.json()
+          }
+        })
+        .catch(error => console.error('Error fetching data:', error));
+    }, [])
 
 
 
--- project/frontend/src/components/FindCourse.jsx
+++ project/frontend/src/components/FindCourse.jsx
@@ -3,17 +3,41 @@ import { FooterCentered } from "./FooterCentered/FooterCentered";
 import { Paper } from "@mantine/core";
 import { InputFindCourse } from "./InputFindCourse/InputFindCourse";
 import { Container } from "@mantine/core";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 
 
 
 const FindCourse = () => {
+    const navigate = useNavigate()
+
+
+  const NotLogin = () => {
+    navigate('/notlogin')
+  }
 
   const containerStyles = {
     position: 'fixed',
     bottom: 0,
     width: '100%',
   };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/users/profile", {method: 'GET',
+    credentials: 'include' })
+      .then(response => {
+        if (response.status === 401){
+            NotLogin()
+        }
+        else {
+            return response.json()
+        }
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, [])
 
     return (
         <div>
